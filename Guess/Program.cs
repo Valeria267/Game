e4b5b39ee2de010a3dc018800c5c4a29780d2897
Валерия <lerik.ru_26@mail.ru>
2016-05-05 22:40:25 +0300

    create new file

---
+++ Guess/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Number;
+
+namespace Guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Game: Guess the number. Guess a number between 1 and 100 ");
+            GNumber v = new GNumber();
+            string s;
+            do
+            {
+                s = v.NumberEquals(Convert.ToInt32(Console.ReadLine()));
+                Console.WriteLine("" + s);
+            }
+            while (s!= null);
+            Console.WriteLine("No attempts. You lose. Number is "+v.Number);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Number/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number
+{
+    public class GNumber
+    {
+        private int number;//задуманное число 
+        private int k = 3;
+        
+
+        public GNumber()
+        {
+           this.number = new Random().Next(0,101);
+        }
+
+        public int Number
+        {
+            get
+            {
+                return number;
+            }
+        }
+
+       
+        public string NumberEquals(int UserNumber)
+        {
+            if (UserNumber == number)
+                return "You won";
+            
+            else
+                  { if ((UserNumber >= 0) && (UserNumber <= 100))
+                {
+                    if (k <= 1)
+                    {
+                        return null;
+                    }
+                    if (UserNumber < number)
+                    { k--; return "More. Remaining " + k + " attempts"; }
+                    else { k--; return "Less. Remaining " + k + " attempts"; }
+                }
+                else return "incorrect values";
+                  }
+         }
+
+    }
+}
